1f2951a9fc8b65dd8df1c1d6e997436e41a09781
samsularifin05 <samsul026@gmail.com>
2023-02-20 16:30:05 +0700

    update

--- src/pages/DataMaster/users/index.tsx
+++ src/pages/DataMaster/users/index.tsx
@@ -3,6 +3,28 @@ import FormDataUser from "./form";
 import TabelUser from "./tabel";
 
 const DataUsers = () => {
+  const handleShareButton = () => {
+    // Check if navigator.share is supported by the browser
+    if (navigator.share) {
+      console.log("Congrats! Your browser supports Web Share API");
+      navigator
+        .share({
+          url: `https://share.toogoodtogo.com/store/1006/milestones/meals-saved/`
+        })
+        .then(() => {
+          // console.log("Sharing successfull");
+          alert("Sharing successfull");
+        })
+        .catch(() => {
+          // console.log("Sharing failed");
+          alert("Sharing failed");
+        });
+    } else {
+      alert("Sorry! Your browser does not support Web Share API");
+
+      console.log("Sorry! Your browser does not support Web Share API");
+    }
+  };
   return (
     <PanelContent
       title="Dashboard"
@@ -11,6 +33,7 @@ const DataUsers = () => {
       headerContent
     >
       <Card title="Data Users">
+        <button onClick={() => handleShareButton()}> kli2k </button>
         <TabelUser />
       </Card>
 
