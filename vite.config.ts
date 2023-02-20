03695fb3b9febdf0221f3906acb8fb97c075f9b9
samsularifin05 <samsul026@gmail.com>
2023-02-20 13:06:05 +0700

    update fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,74 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { ManifestOptions, VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 import svgrPlugin from "vite-plugin-svgr";
+import htmlMinifier from "vite-plugin-html-minifier";
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: "development",
+  base: "/",
+  includeAssets: ["favicon.svg"],
+  manifest: {
+    name: "Admin Lte",
+    short_name: "Admin Lte",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "pwa-192x192.png", // <== don't add slash, for testing
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "/pwa-512x512.png", // <== don't remove slash, for testing
+        sizes: "512x512",
+        type: "image/png"
+      },
+      {
+        src: "pwa-512x512.png", // <== don't add slash, for testing
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ]
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === "true",
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: "module",
+    navigateFallback: "index.html"
+  }
+};
+
+const replaceOptions = { __DATE__: new Date().toISOString() };
+const claims = process.env.CLAIMS === "true";
+const reload = process.env.RELOAD_SW === "true";
+const selfDestroying = process.env.SW_DESTROY === "true";
+
+if (process.env.SW === "true") {
+  pwaOptions.srcDir = "src";
+  pwaOptions.filename = claims ? "claims-sw.ts" : "prompt-sw.ts";
+  pwaOptions.strategies = "injectManifest";
+  (pwaOptions.manifest as Partial<ManifestOptions>).name =
+    "PWA Inject Manifest";
+  (pwaOptions.manifest as Partial<ManifestOptions>).short_name = "PWA Inject";
+}
+
+if (claims) pwaOptions.registerType = "autoUpdate";
+
+if (reload) {
+  // @ts-expect-error just ignore
+  replaceOptions.__RELOAD_SW__ = "true";
+}
+
+if (selfDestroying) pwaOptions.selfDestroying = selfDestroying;
+
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [
       react(),
+      VitePWA(pwaOptions),
+      htmlMinifier({
+        minify: true
+      }),
       svgrPlugin({
         svgrOptions: {
           icon: true
@@ -21,7 +85,8 @@ export default defineConfig(({ command, mode }) => {
     build: {
       emptyOutDir: true,
       outDir: "build",
-      sourcemap: true,
+      sourcemap: process.env.SOURCE_MAP === "true",
+      chunkSizeWarningLimit: 1600,
       rollupOptions: {
         output: {
           chunkFileNames: "assets/js/[name]-[hash].js",
